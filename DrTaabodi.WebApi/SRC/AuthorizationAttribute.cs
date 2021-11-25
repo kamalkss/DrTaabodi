8e33fc8ccc440dce521eb20f77427d6411b9dfcc
Mahdi Hasanpour <mh.hasnapour@gmail.com>
2021-11-25 17:54:05 +0330

    authoriztion attribute

---
+++ DrTaabodi.WebApi/SRC/AuthorizationAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace DrTaabodi.WebApi.SRC
+{
+    public class AuthorizationAttribute : Attribute, IAuthorizationFilter, IOrderedFilter
+    {
+        public int Order => 0;
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            context.Result = new UnauthorizedResult();
+        }
+    }
+}
